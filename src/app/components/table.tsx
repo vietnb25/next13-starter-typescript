3a911ed87f37ca5dbd9231969d5f3b65d23358c3
vietnb25 <nhunnhun2503@gmail.com>
2025-07-09 17:29:55 +0700

    Update layout

--- src/app/components/table.tsx
+++ src/app/components/table.tsx
@@ -10,36 +10,44 @@ const TableContent = (props: IProps) => {
     console.log("check: ", blogs);
 
     return (
-        <Table striped bordered hover>
-            <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Title</th>
-                    <th>Author</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                {blogs.map(blog => {
-                    return (
-                        <tr key={blog.id}>
-                            <td>{blog.id}</td>
-                            <td>{blog.title}</td>
-                            <td>{blog.author}</td>
-                            <td >
-                                <Button variant="primary">ADD</Button>
-                                <Button variant="warning" className='mx-3'>EDIT</Button>
-                                <Button variant="danger">DELETE</Button>
-                            </td>
+        <>
+            <div
+                className='mb-3'
+                style={{ display: "flex", justifyContent: "space-between" }}>
+                <h3>Table Blogs</h3>
+                <Button variant='secondary'>ADD NEW</Button>
+            </div>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Title</th>
+                        <th>Author</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {blogs.map(blog => {
+                        return (
+                            <tr key={blog.id}>
+                                <td>{blog.id}</td>
+                                <td>{blog.title}</td>
+                                <td>{blog.author}</td>
+                                <td >
+                                    <Button variant="primary">VIEW </Button>
+                                    <Button variant="warning" className='mx-3'>EDIT</Button>
+                                    <Button variant="danger">DELETE</Button>
+                                </td>
 
 
 
-                        </tr>)
-                })}
+                            </tr>)
+                    })}
 
 
-            </tbody>
-        </Table>
+                </tbody>
+            </Table>
+        </>
     );
 }
 
